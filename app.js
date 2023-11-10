580c57303d95b360fbc5adc16f6007ec5eec3a3b
yunkihong-dev <rickyhong0130@gmail.com>
2023-11-10 16:23:39 +0900

    before pull

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ app.listen(PORT, function() {
     console.log('Listening on port: ', PORT);
     console.log('hi')
 }); 
+
+app.get('/main', function(req, res) {
+	res.send('main 페이지입니다.');
+});
+
+app.get('/test', function(req, res) {
+	res.send('test 페이지입니다.');
+});
