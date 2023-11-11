646d473826969f82e98941b584a7c91209d0c21b
jjsnxsxou <ys10305348@gmail.com>
2023-11-11 11:22:59 +0900

    modified by jjs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const PORT = 8080;
 app.listen(PORT, function() {
+    console.log('server start');
     console.log('Listening on port: ', PORT);
     console.log('hi')
 }); 
